0fdd4b353065d0e12ef171fcfc0cdbced267c1d0
Brown <Boris.Brown@nielsen.com>
2021-04-12 14:26:06 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
-}
+  sourceList: Satellite[];
+  constructor() {
+      let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+      // TODO: loop over satellites
+      // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+      // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+      window.fetch(satellitesUrl).then(function(response) {
+        response.json().then(function(data){
+          //console.log(data)
+          console.log(data.satellites[3]);
+          //fetchedSatellites.push('abc123')
+          for (let i = 0; i <= data.satellites.length; i++) {
+             let satellite = new Satellite(data.satellites[i].name, data.satellites[i].type, data.satellites[i].launchDate, data.satellites[i].orbitType, data.satellites[i].operational);
+             this.sourceList.push(satellite)
+        }
+        })
+      })
+    }
+  }
+
+
+      // window.fetch(satellitesUrl).then(function(response) {
+      //    response.json().then(function() {
+      //      for (let i = 0; i <= response["satellites"].length; i++) {
+      //       fetchedSatellites.push(response["satellites"][i]); 
+      //       let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+      //       this.sourceList.push(satellite);
+      //      }
+      //    }.bind(this));
+      // }.bind(this));
+
---
+++ src/app/satellite.ts
@@ -0,0 +1,14 @@
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+    constructor(name: string, orbitType: string, type: string, launchDate: string, operational: boolean) {
+        this.name=name,
+        this.orbitType=orbitType,
+        this.type=type,
+        this.launchDate=launchDate,
+        this.operational=operational
+    }
+}
